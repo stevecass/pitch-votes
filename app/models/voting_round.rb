6db3b4ed9f5ef2e87162f058cbfce3fb1fc40400
Steven Cassidy <steven@mumsnet.com>
2014-09-23 19:55:25 -0400

    Fix voting round numbering

--- app/models/voting_round.rb
+++ app/models/voting_round.rb
@@ -8,7 +8,7 @@ class VotingRound < ActiveRecord::Base
 
   def self.create_with_candidates(day_id, candidate_ids)
     vr = VotingRound.new({ day_id: day_id })
-    vr.round_number = VotingRound.max_for_day(day_id)
+    vr.round_number = 1 + VotingRound.max_for_day(day_id)
     vr.save
     candidate_ids.each do |id|
       vr.voting_round_candidates << VotingRoundCandidate.create({ pitch_id: id})
