d3e0c910649ffcd021f9e9c1a346ed1cbbc1fc9e
Steven Cassidy <steven@mumsnet.com>
2014-09-25 11:27:05 -0400

    Add delegates to candidate

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,6 +1,9 @@
 class Candidate < ActiveRecord::Base
   belongs_to :voting_round
+  delegate :round_number, to: :voting_round, allow_nil: true, prefix: false
   belongs_to :pitch
+  delegate :name, to: :pitch, allow_nil: true, prefix: true
   has_many :votes
+
 end
 
