1e2042eb77d05db09651936e5d087b700b109d03
Steven Cassidy <steven@mumsnet.com>
2014-09-25 11:49:35 -0400

    Add delegation on votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,9 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
+  delegate :name, to: :user, allow_nil: true, prefix: true
   belongs_to :voting_round
   belongs_to :candidate
+  delegate :name, :proposer, to: :candidate, allow_nil: true, prefix: true
   
   def self.delete_for_user_and_round(user, round)
     Vote.delete_all(user: user, voting_round: round)
