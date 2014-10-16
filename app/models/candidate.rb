db09ac11479c4a763d8eb9f2ed9c156bed6c5a9d
Steven Cassidy <steven@mumsnet.com>
2014-10-16 13:49:29 -0400

    Delegate description from candidate to pitch

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -2,7 +2,7 @@ class Candidate < ActiveRecord::Base
   belongs_to :voting_round
   delegate :round_number, to: :voting_round, allow_nil: true, prefix: false
   belongs_to :pitch
-  delegate :name, :proposer, to: :pitch, allow_nil: true, prefix: false
+  delegate :name, :proposer, :description, to: :pitch, allow_nil: true, prefix: false
   has_many :votes, :dependent => :restrict_with_exception
 
   def self.pitch_ids_for_round round_id
